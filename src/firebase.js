add49e770790bbc4192bc5f1704b8f89924bb05b
Benjamin Mah <84874055+benjaminmah@users.noreply.github.com>
2023-06-08 00:12:19 -0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,11 +3,11 @@ import "firebase/auth";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const auth = firebase.initializeApp({
-    apiKey: "AIzaSyDNbTs0qewg2vkfM0gf8b8-I5UbKaabxr0",
-    authDomain: "heychat-0.firebaseapp.com",
-    projectId: "heychat-0",
-    storageBucket: "heychat-0.appspot.com",
-    messagingSenderId: "383835837038",
-    appId: "1:383835837038:web:1fb981ede53d4337f50c7f",
-    measurementId: "G-HFD11MCMC3"
+    apiKey: "<REDACTED>",
+    authDomain: "<REDACTED>",
+    projectId: "<REDACTED>",
+    storageBucket: "<REDACTED>",
+    messagingSenderId: "<REDACTED>",
+    appId: "<REDACTED>",
+    measurementId: "<REDACTED>"
   }).auth();
